b136f713d093f912ffb80da8113c044c64d20101
CollierTR <tristancollier777@gmail.com>
2026-02-04 13:10:26 -0600

    add write file function

---
+++ functions/write_file.py
@@ -0,0 +1,29 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        working_dir_path = os.path.abspath(working_directory)
+        target_file_path = os.path.normpath(os.path.join(working_dir_path, file_path))
+
+        # Will be True or False
+        valid_target_dir = os.path.commonpath([working_dir_path, target_file_path]) == working_dir_path
+
+        if not valid_target_dir:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if os.path.isdir(target_file_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+
+        abs_file_path = os.path.abspath(target_file_path)
+        os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
+
+        # Do something here...
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+
+
+        return content
+    except Exception as e:
+        return f"Error: {e}"
